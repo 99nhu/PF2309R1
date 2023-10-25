010ba3b227953e3fe0b7e1c830bd405e78eb9966
99nhu <vongocquynhnhu49@gmail.com>
2023-10-25 22:42:33 +0900

    Update file lesson 7

---
+++ Buoi 7 - 11-10-2023/script.js
@@ -0,0 +1,5 @@
+//nhap vao mot so nguyen n va in ra binh phuong cua no.
+let n = prompt("Please input a number: ");
+
+let result = n ** 2;
+alert(result);
---
+++ Buoi 7 - 11-10-2023/script2.js
@@ -0,0 +1,8 @@
+let length = parent(prompt("Please input length of rectangle:"));
+let width = parent(prompt("Please input width of rectangle:"));
+
+let perimeter = (length + width) * 2;
+let area = length * width;
+
+alert(perimeter);
+alert(area);
