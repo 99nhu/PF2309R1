01856f5c469a4c7998fb39839eeca36f402ad32f
99nhu <vongocquynhnhu49@gmail.com>
2023-11-16 14:15:10 +0900

    Update homework

---
+++ Bai tap Learn/PF-W02/sd_toantu.js
@@ -0,0 +1,6 @@
+let inputWidth = parseInt(prompt("Enter the width"));
+let inputHeight = parseInt(prompt("Enter the height"));
+
+let area = inputWidth * inputHeight;
+document.write("The area is: " + area);
+
